5750f26441e0237e0d58e4b194d16afaa3c46eb8
Mayur Chopra <mayur5c@users.noreply.github.com>
2023-01-07 12:03:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+// Get a reference to the textbox and the h3 element
+const textbox = document.getElementById('evaluatedText');
+const letterCount = document.getElementById('letterCount');
+
+// Add an event listener to the textbox that updates the letter count when the text is changed
+textbox.addEventListener('input', function() {
+  // Get the current text in the textbox
+  const text = textbox.value;
+
+  // Update the letter count in the h3 element
+  letterCount.innerHTML = text.length;
+});
